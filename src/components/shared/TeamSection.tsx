20dae7fd1cf8f06726879226ceb8f6bad7e4931e
Aldrich <nocheblanca92@gmail.com>
2025-10-16 19:43:59 -0600

    Feat:arreglar imagenes de abodagos

--- src/components/shared/TeamSection.tsx
+++ src/components/shared/TeamSection.tsx
@@ -38,7 +38,7 @@ const members = [
     name: "María Fernanda Chávez Quiroz",
     role: "Asociada - Litigio Constitucional",
     avatar:
-      "https://images.unsplash.com/photo-1623880840102-7df0a9f3545b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
+      "https://images.unsplash.com/photo-1662104935883-e9dd0619eaba?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
     link: "#",
   },
 ];
